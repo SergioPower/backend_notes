e647ca60e9f748c6daf31811245f1c0834b471a1
Sergio Guzman <serguz95444@gmail.com>
2025-01-09 11:20:13 -0600

    Agregar soporte para variables de entorno y conexión a MongoDB

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const app = express()
+
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-
-const password = process.argv[2]
-const url =
-  `mongodb+srv://serguzdev99:${password}@cluster0.7w0u8.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
@@ -104,7 +85,7 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
