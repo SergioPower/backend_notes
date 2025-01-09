1f2f3a547c80c4e97f8297f1f70fe5c7f329f6ee
Sergio Guzman <serguz95444@gmail.com>
2025-01-09 11:06:01 -0600

    Agregar conexión a MongoDB y modelo de notas

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
 
-app.use(cors()) 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
+const password = process.argv[2]
+const url =
+  `mongodb+srv://serguzdev99:${password}@cluster0.7w0u8.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
 let notes = [
   {
-    id: "1",
-    content: "HTML is easy",
+    id: '1',
+    content: 'HTML is easy',
     important: true
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
+    id: '2',
+    content: 'Browser can execute only JavaScript',
     important: false
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
     important: true
   }
 ]
-app.get('/', (request, response) => {     // primer parametro contiene toda la información de la solicitud,
-                                          // el segunto parametro de respuesta se utiliza para definir 
-                                          // cómo se responde a la solicitud
-  response.send('<h1>Hello World!</h1>')  // espera un acadena de texto
+app.get('/', (request, response) => { // primer parametro contiene toda la información de la solicitud,
+  // el segunto parametro de respuesta se utiliza para definir
+  // cómo se responde a la solicitud
+  response.send('<h1>Hello World!</h1>') // espera un acadena de texto
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
-
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
-  
 
   if (note) {
     response.json(note)
@@ -50,16 +73,15 @@ app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter(note => note.id !== id)
   response.status(204).end()
-  console.log(notes);
-  
+  console.log(notes)
 })
 
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
-  console.log(maxId);
-  
+  console.log(maxId)
+
   return maxId + 1
 }
 
@@ -67,15 +89,15 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
   const note = {
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
+    id: generateId()
   }
 
   notes = notes.concat(note)
